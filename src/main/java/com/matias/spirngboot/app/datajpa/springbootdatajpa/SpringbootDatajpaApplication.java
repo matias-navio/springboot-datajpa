7344b432a67beef253fd9fa7ca64349472b2cffd
Matias Navio <naviomatias2@gmail.com>
2024-03-06 08:51:13 -0300

    find de listas y de a un elemento, create

--- src/main/java/com/matias/spirngboot/app/datajpa/springbootdatajpa/SpringbootDatajpaApplication.java
+++ src/main/java/com/matias/spirngboot/app/datajpa/springbootdatajpa/SpringbootDatajpaApplication.java
@@ -1,11 +1,14 @@
 package com.matias.spirngboot.app.datajpa.springbootdatajpa;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.Scanner;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.matias.spirngboot.app.datajpa.springbootdatajpa.entities.Person;
 import com.matias.spirngboot.app.datajpa.springbootdatajpa.repositories.PersonRepository;
@@ -23,7 +26,72 @@ public class SpringbootDatajpaApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 
+		// list();
+		// findOde();
+		create();
+	}
+
+	@Transactional
+	public void create(){
+		Scanner scanner = new Scanner(System.in);
+		String name = scanner.next();
+		String lastName = scanner.next();
+		String programmingLanguage = scanner.next();
+
+		Person person = new Person(null, name, lastName, programmingLanguage);
+
+		Person newPerson = repository.save(person);
+		System.out.println(newPerson);
+
+		repository.findById(newPerson.getId()).ifPresent(System.out::println);
+	}
+
+	// se anota asi los métodos que no modifican la lista
+	@Transactional(readOnly = true)
+	public void findOde(){
+		Person person = null;
+		Optional<Person> optionalPerson = repository.findOne(1L);
+		// esto es en caso de que la persona no exista, con esto lanza una excepcion
+		String namePersona = optionalPerson.get().getLastName();
+		// validamos si esta presente y la mostramos
+		if(optionalPerson.isPresent()){
+			person = optionalPerson.get();
+		}
+		System.out.println(person);
+
+		// esta linea sola hace lo mismo que todo lo de arriba
+		repository.findOne(2L).ifPresent(persons -> System.out.println(persons));
+		// Busqueda de uno por nombre
+		repository.findOneName("Giuliano").ifPresent(persons -> System.out.println(persons));
+		// buscamos un usuario por una coinsidencia en el nombre, es decir, devuelve en base a si encuentra algo parecido para adelante y atras
+		repository.findOneLikeName("Ju").ifPresent(System.out::println);
+		// es igual que el de arriba pero ya viene integrado
+		repository.findByNameContaining("qui").ifPresent(System.out::println);
+
+	}
+
+	@Transactional(readOnly = true)
+	public void list(){
+
 		List<Person> persons = (List<Person>) repository.findAll();
+		System.out.println("\n Estos solo son todos ");
 		persons.stream().forEach(person -> System.out.println(person));
+
+		List<Person> personsByLanguage = (List<Person>) repository.buscarByProgrammingLanguage("Java");
+		System.out.println("\n Estos solo son los de JAVA");
+		personsByLanguage.stream().forEach(person -> System.out.println(person));
+
+		List<Person> personsByLanguageAndName = (List<Person>) repository.buscarByProgrammingLanguageAndName("Java", "Matias");
+		System.out.println("\n Estos son los de JAVA con nombre Matias");
+		personsByLanguageAndName.stream().forEach(person -> System.out.println(person));
+
+		List<Object[]> personValues = repository.obtenerPersonData();
+		System.out.println("\n Datos de las personas");
+		personValues.stream().forEach(person -> System.out.println(person[0] + " " + person[1] +  " es exterto en " + person[2]));
+
+		List<Object[]> personValuesByProgrammingLanguage = repository.obtenerPersonDataByProgrammingLanguage("Java");
+		System.out.println("\n Filtrar por lenguaje");
+		personValuesByProgrammingLanguage.stream().forEach(person -> System.out.println(person[0] + " es experto/a en " + person[1]));
+
 	}
 }
--- src/main/java/com/matias/spirngboot/app/datajpa/springbootdatajpa/repositories/PersonRepository.java
+++ src/main/java/com/matias/spirngboot/app/datajpa/springbootdatajpa/repositories/PersonRepository.java
@@ -1,9 +1,38 @@
 package com.matias.spirngboot.app.datajpa.springbootdatajpa.repositories;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.matias.spirngboot.app.datajpa.springbootdatajpa.entities.Person;
 
 public interface PersonRepository extends CrudRepository<Person, Long>{
+
+    // la consulta personalizada se hace sobre la clase entity, y no sobre la tabla
+    @Query("select p from Person p where p.programmingLanguage = ?1")
+    List<Person> buscarByProgrammingLanguage(String prograamingLanguage);
+
+    @Query("select p from Person p where p.programmingLanguage = ?1 and p.name= ?2")
+    List<Person> buscarByProgrammingLanguageAndName(String prograamingLanguage, String name);
+
+    @Query("select p.name, p.lastName, p.programmingLanguage from Person p")
+    List<Object[]> obtenerPersonData();
+
+    @Query("select p.name, p.programmingLanguage from Person p where p.programmingLanguage = ?1")
+    List<Object[]> obtenerPersonDataByProgrammingLanguage(String programmingLanguage);
+
+    @Query("select p from Person p where p.id = ?1")
+    Optional<Person> findOne(Long id);
+
+    @Query("select p from Person p where p.name = ?1")
+    Optional<Person> findOneName(String name);
+
+    @Query("select p from Person p where p.name like %?1%")
+    Optional<Person> findOneLikeName(String name);
+
+    // este es igual al de arriba, pero este ya está integrado
+    Optional<Person> findByNameContaining(String name);
 
 }
